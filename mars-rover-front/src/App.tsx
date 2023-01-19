609f8a1ebaa0725429d2c78a796535807cefbe50
Vitor <73979580+Blackklegend@users.noreply.github.com>
2023-01-19 02:38:07 -0300

    set up front

--- mars-rover-front/src/App.tsx
+++ mars-rover-front/src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import rover from './assets/mars-rover.png'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-red-500 customfont h-screen w-screen flex justify-center">
+      <div className="mt-3 border rounded mx-2 p-0 mb-28 resizeable w-9/12">
+        <img 
+          src={rover} 
+          className="rover ml"
+          />
+      </div>
+
+      <div className="text-black absolute bottom-0 place-self-center grid w-64 place-items-center">
+        <div className="bg-white rounded-t-lg text-dark p-2 d-block w-2/3 border-bottom-0 rounded-bottom-0 row-span-1 text-center">
+          <p className="inline text-xl">X: 0 | Y: 0</p>
+        </div>
+        <div className="bg-white row-span-1 min-w-full block rounded-t-lg text-center">
+            <button className="p-2 hover:bg-gray-200 rounded-md m-1 min-w-[55px]">
+              <i className="fa-solid fa-rotate-left text-4xl"></i>
+            </button>
+            <button className="p-2 hover:bg-gray-200 rounded-md m-1 min-w-[55px]">
+              <i className="fa-solid fa-arrow-up text-4xl"></i>
+            </button>
+            <button className="p-2 hover:bg-gray-200 rounded-md m-1 min-w-[55px]">
+              <i className="fa-solid fa-rotate-right text-4xl"></i>
+            </button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
